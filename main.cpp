2a6c1588d20be9ed147bb4469f23db30f8774e5f
michelleg27 <mgomez48@mail.csuchico.edu>
2018-02-27 11:45:32 -0800

    Fixed

--- main.cpp
+++ main.cpp
@@ -41,9 +41,10 @@ while (input != 'q' && input != 'Q');
      
     if( input == 'q' || input =='Q')
     {
+        cout<<"==ITEM TO BUY=="<<endl;
         cout<<"1 "<<list[0]<<endl; 
         cout<<"2 "<<list[1]<<endl; 
-        cout<<"3 "<<item[2]<<endl;
+        cout<<"3 "<<list[2]<<endl;
         cout<<"4 "<<list[3]<<endl; 
         cout<<"5 "<<list[4]<<endl; 
 
